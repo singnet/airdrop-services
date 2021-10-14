c896feea06b7419aa0060a707dd6cfa4ff3ba7fd
Rajeev <scraptorajeev@gmail.com>
2021-10-14 08:51:24 +0530

    Adding node_modules for explicityly

--- airdrop/constants.py
+++ airdrop/constants.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-COMMON_CNTRCT_PATH = '../resources/node_modules/singularitynet-airdrop-contracts'
+COMMON_CNTRCT_PATH = './resources/node_modules/singularitynet-airdrop-contracts'
 AIRDROP_ADDR_PATH = COMMON_CNTRCT_PATH + '/networks/SingularityAirdrop.json'
 
 
