7629e6741f4acc8274dddefe216ef14c53c9db7b
Rajeev <scraptorajeev@gmail.com>
2021-10-27 19:10:56 +0530

    Variable name change

--- airdrop/domain/models/airdrop_claim_history.py
+++ airdrop/domain/models/airdrop_claim_history.py
@@ -1,5 +1,5 @@
 class AirdropClaimHistory:
-    def __init__(self, airdrop_id, airdrop_window_id, address, txn_status, txn_hash, claimable_amount, claimed_on, user_registration):
+    def __init__(self, airdrop_id, airdrop_window_id, address, txn_status, txn_hash, claimable_amount, claimed_on, airdrop_window_registration):
         self._airdrop_id = airdrop_id
         self._airdrop_window_id = airdrop_window_id
         self._address = address
@@ -7,7 +7,8 @@ class AirdropClaimHistory:
         self._txn_hash = txn_hash
         self._claimable_amount = claimable_amount
         self._claimed_on = claimed_on
-        self._user_registration = user_registration[0]
+        self._registered_at = str(airdrop_window_registration.registered_at)
+        self._is_eligible = airdrop_window_registration.is_eligible
 
     def to_dict(self):
         return {
@@ -18,6 +19,6 @@ class AirdropClaimHistory:
             "txn_status": self._txn_status,
             "claimable_amount": self._claimable_amount,
             "claimed_on": self._claimed_on,
-            "registered_at": str(self._user_registration.registered_at),
-            "is_eligible": self._user_registration.is_eligible
+            "registered_at": self._registered_at,
+            "is_eligible": self._is_eligible
         }
