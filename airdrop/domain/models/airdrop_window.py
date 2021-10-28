e8697ca391f8c75b1f379c2fb50c382d0a50a692
Rajeev <scraptorajeev@gmail.com>
2021-10-28 08:58:52 +0530

    Airdrop window rules update

--- airdrop/domain/models/airdrop_window.py
+++ airdrop/domain/models/airdrop_window.py
@@ -1,8 +1,9 @@
 from airdrop.domain.models.airdrop_window_timeline import AirdropWindowTimeline
+from airdrop.domain.models.airdrop_window_rule import AirdropWindowRule
 
 
 class AirdropWindow:
-    def __init__(self, airdrop_id, window_id, window_name,  description, registration_start_period, registration_end_period, total_airdrop_tokens, airdrop_window_timeline, airdrop_window_claim_start_period, airdrop_window_claim_end_period):
+    def __init__(self, airdrop_id, window_id, window_name,  description, registration_start_period, registration_end_period, total_airdrop_tokens, airdrop_window_timeline, airdrop_window_claim_start_period, airdrop_window_claim_end_period, airdropwindow_rules):
         self._airdrop_id = airdrop_id
         self._window_id = window_id
         self._window_name = window_name
@@ -13,6 +14,14 @@ class AirdropWindow:
         self._airdrop_window_timeline = airdrop_window_timeline
         self._airdrop_window_claim_start_period = airdrop_window_claim_start_period
         self._airdrop_window_claim_end_period = airdrop_window_claim_end_period
+        self._airdropwindow_rules = airdropwindow_rules
+
+    def get_airdrop_window_rules(self):
+        return [
+            AirdropWindowRule(
+                rule.title, rule.rule).to_dict()
+            for rule in self._airdropwindow_rules
+        ]
 
     def get_airdrop_window_timeline(self):
         return [
@@ -32,5 +41,6 @@ class AirdropWindow:
             "airdrop_window_total_tokens": self._total_airdrop_tokens,
             "airdrop_window_claim_start_period": self._airdrop_window_claim_start_period,
             "airdrop_window_claim_end_period": self._airdrop_window_claim_end_period,
-            "airdrop_window_timeline": self.get_airdrop_window_timeline()
+            "airdrop_window_timeline": self.get_airdrop_window_timeline(),
+            "airdrop_window_eligibility_rules": self.get_airdrop_window_rules()
         }
