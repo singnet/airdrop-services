26179f1d448b810453085b9af7748b95b8dfe24f
Rajeev <scraptorajeev@gmail.com>
2021-11-03 15:55:56 +0530

    Add | Rewards added data

--- airdrop/domain/models/airdrop_window_eligibility.py
+++ airdrop/domain/models/airdrop_window_eligibility.py
@@ -1,5 +1,5 @@
 class AirdropWindowEligibility:
-    def __init__(self, airdrop_id, airdrop_window_id, address, is_eligible_user, is_already_registered, is_airdrop_window_claimed, airdrop_claim_status, reject_reason):
+    def __init__(self, airdrop_id, airdrop_window_id, address, is_eligible_user, is_already_registered, is_airdrop_window_claimed, airdrop_claim_status, reject_reason, rewards_awards):
         self._is_eligible_user = is_eligible_user
         self._is_already_registered = is_already_registered
         self._is_airdrop_window_claimed = is_airdrop_window_claimed
@@ -8,6 +8,7 @@ class AirdropWindowEligibility:
         self._airdrop_id = airdrop_id
         self._airdrop_window_id = airdrop_window_id
         self._reject_reason = reject_reason
+        self._rewards_awards = rewards_awards
 
     def to_dict(self):
         return {
@@ -18,5 +19,6 @@ class AirdropWindowEligibility:
             "user_address": self._address,
             "airdrop_id": self._airdrop_id,
             "airdrop_window_id": self._airdrop_window_id,
-            "reject_reason": self._reject_reason
+            "reject_reason": self._reject_reason,
+            "rewards_awards": self._rewards_awards
         }
