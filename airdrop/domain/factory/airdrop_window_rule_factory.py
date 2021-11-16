b134bb577e5a607e14a1d1304265796f3930440e
Rajeev <scraptorajeev@gmail.com>
2021-11-16 09:41:29 +0530

    Delete airdrop_window_rule_factory.py

--- airdrop/domain/factory/airdrop_window_rule_factory.py
+++
@@ -1,8 +0,0 @@
-from airdrop.domain.models.airdrop_window import AirdropWindow
-
-
-class AirdropWindowRuleFactory:
-    @staticmethod
-    def convert_airdrop_window_rule_model_to_entity_model(window):
-        return AirdropWindow(window.airdrop_id, window.id, window.airdrop_window_name,
-                             window.description, str(window.registration_start_period), str(window.registration_end_period), window.total_airdrop_tokens).to_dict()
