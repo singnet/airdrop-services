3d5ccb49ae44bddc3734232597739013c214470e
anandrgitnirman <NIrman$123>
2022-01-14 16:33:10 +0530

    Any Amount  , like stakable amount , rewards to be claimed , total eligibility rewards will all be returned as a string ( the front end expects the same) and hence not changing this behaviour. Please note the airdrop_id and the airdrop_window_id returned were values on what was requested , which were strings, hence they did not need the str conversion

--- airdrop/domain/models/airdrop_claim.py
+++ airdrop/domain/models/airdrop_claim.py
@@ -1,5 +1,5 @@
 class AirdropClaim:
-    def __init__(self, airdrop_id, window_id, address, signature, amount, token_address, contract_address, staking_contract_address):
+    def __init__(self, airdrop_id, window_id, address, signature, amount, token_address, contract_address, staking_contract_address,total_eligibility_amount):
         self._airdrop_id = airdrop_id
         self._window_id = window_id
         self._address = address
@@ -8,6 +8,7 @@ class AirdropClaim:
         self._token_address = token_address
         self._contract_address = contract_address
         self._staking_contract_address = staking_contract_address
+        self._total_eligibility_amount = total_eligibility_amount
 
     def to_dict(self):
         return {
@@ -15,8 +16,9 @@ class AirdropClaim:
             "airdrop_window_id": self._window_id,
             "user_address": self._address,
             "signature": self._signature,
-            "claimable_amount": self._amount,
+            "claimable_amount": str(self._amount),
             "token_address": self._token_address,
             "contract_address": self._contract_address,
-            "staking_contract_address": self._staking_contract_address
+            "staking_contract_address": self._staking_contract_address,
+            "total_eligibility_amount": str(self._total_eligibility_amount)
         }
