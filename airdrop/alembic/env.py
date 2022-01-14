e9074547613a864e33bae04b44d1767ac3b6a195
Rajeev <scraptorajeev@gmail.com>
2022-01-14 17:58:29 +0530

    Change datatype

--- airdrop/alembic/env.py
+++ airdrop/alembic/env.py
@@ -51,6 +51,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
     )
 
     with context.begin_transaction():
@@ -71,7 +72,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
 
         with context.begin_transaction():
             context.run_migrations()
