4d1b6efb310cf0fb60c0cf83b34b140575199ee1
karthi96 <karthikeyandon105@gmail.com>
2022-11-11 09:06:58 +0530

    updated testcase

--- airdrop/testcases/functional_testcases/test_loyality_reward.py
+++ airdrop/testcases/functional_testcases/test_loyality_reward.py
@@ -1,11 +1,17 @@
 import json
+from decimal import Decimal
 from unittest import TestCase
+from unittest.mock import patch
 
+from airdrop.infrastructure.models import UserReward
+from airdrop.infrastructure.repositories.user_reward_repository import UserRewardRepository
 from airdrop.job.eligibility import process_loyalty_airdrop_reward_eligibility
 from airdrop.testcases.functional_testcases.load_test_data import clear_database, load_airdrop_data, \
     load_airdrop_window_data
 from airdrop.testcases.functional_testcases.loyalty_airdrop_test_data import LoyaltyAirdropData, \
-    LoyaltyAirdropWindow1Data, LoyaltyAirdropWindow2Data
+    LoyaltyAirdropWindow1Data, LoyaltyAirdropWindow2Data, LoyaltyAirdropWindow3Data
+
+user_reward_repository = UserRewardRepository()
 
 
 class TestLoyaltyAirdropServices(TestCase):
@@ -15,8 +21,10 @@ class TestLoyaltyAirdropServices(TestCase):
         self.airdrop = load_airdrop_data(LoyaltyAirdropData)
         self.loyalty_airdrop_window1 = load_airdrop_window_data(self.airdrop.id, LoyaltyAirdropWindow1Data)
         self.loyalty_airdrop_window2 = load_airdrop_window_data(self.airdrop.id, LoyaltyAirdropWindow2Data)
+        self.loyalty_airdrop_window3 = load_airdrop_window_data(self.airdrop.id, LoyaltyAirdropWindow3Data)
 
-    def test_process_loyalty_airdrop_reward_eligibility(self):
+    @patch("airdrop.job.reward_processors.loyalty_reward_processor.LoyaltyEligibilityProcessor.get_eligible_users")
+    def test_process_loyalty_airdrop_reward_eligibility(self, mock_get_eligible_users):
         event = dict()
 
         # Empty input
@@ -50,13 +58,26 @@ class TestLoyaltyAirdropServices(TestCase):
         assert (response_body["message"], "'Validation failed'")
 
         # newer window id provided because claim has not been already started
-        event = {"airdrop_id": self.airdrop.id, "window_id": self.loyalty_airdrop_window2.id}
+        event = {"airdrop_id": self.airdrop.id, "window_id": self.loyalty_airdrop_window3.id}
 
+        mock_get_eligible_users.return_value = []
         response = process_loyalty_airdrop_reward_eligibility(event, None)
         assert (response["statusCode"] == 200)
         response_body = json.loads(response["body"])
-        assert (response_body["message"], "'Validation failed'")
+        assert (response_body["message"], "0 users are eligible for claim ")
+
+        mock_get_eligible_users.return_value = [
+            {'staker_address': '0x00Aee4E1698Fe0829F4663CBef571c948160B858', 'wallet_address': None,
+             'staker_balance': 3016982005590, 'wallet_balance': None, 'is_contract': None}]
+        response = process_loyalty_airdrop_reward_eligibility(event, None)
+        assert (response["statusCode"] == 200)
+        response_body = json.loads(response["body"])
+        assert (response_body["message"], "1 users are eligible for claim ")
+
+        user_reward = user_reward_repository.session.query(UserReward).filter(UserReward.airdrop_id == self.airdrop.id,
+                                                                              UserReward.airdrop_window_id == self.loyalty_airdrop_window3.id,
+                                                                              UserReward.address == "0x00Aee4E1698Fe0829F4663CBef571c948160B858").first()
+        assert (user_reward.rewards_awarded, Decimal(22926589306))
 
     def tearDown(self):
         clear_database()
-updat
