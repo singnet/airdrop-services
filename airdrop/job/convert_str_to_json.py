3d4e116db16bed92234e5dcf70453d27c37bbe40
mabredin <mbredin@bk.ru>
2025-03-27 15:50:50 +0300

    Add a function to change the address format

--- airdrop/job/convert_str_to_json.py
+++ airdrop/job/convert_str_to_json.py
@@ -1,7 +1,10 @@
 import json
 
+from web3 import Web3
+
 from airdrop.infrastructure.models import UserRegistration
 from airdrop.infrastructure.repositories.user_registration_repo import UserRegistrationRepository
+from airdrop.utils import Utils
 from common.logger import get_logger
 
 logger = get_logger(__name__)
@@ -18,7 +21,7 @@ class ConverterFromStrToJSON:
                     f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
         if self.address:
             _, registration = UserRegistrationRepository().get_user_registration_details(address=self.address,
-                                                                                          airdrop_window_id=self._window_id)
+                                                                                         airdrop_window_id=self._window_id)
             registrations = [registration]
         else:
             _, registrations = UserRegistrationRepository().get_user_registration_details(airdrop_window_id=self._window_id)
@@ -47,6 +50,54 @@ class ConverterFromStrToJSON:
         if not registrations:
             raise Exception("0 registrations received from db")
         self.change_signature_details(registrations)
+
+        return (f"{len(registrations)} registrations changed on "
+                f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
+
+
+class ChangerAddressFormat:
+    def __init__(self, airdrop_id, window_id):
+        self._airdrop_id = airdrop_id
+        self._window_id = window_id
+        self.address = None
+
+    def receive_all_registrations(self) -> list[UserRegistration]:
+        logger.info("Processing the receiving all registrations for the "
+                    f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
+        if self.address:
+            _, registration = UserRegistrationRepository().get_user_registration_details(address=self.address,
+                                                                                         airdrop_window_id=self._window_id)
+            registrations = [registration]
+        else:
+            _, registrations = UserRegistrationRepository().get_user_registration_details(airdrop_window_id=self._window_id)
+        return registrations
+
+    def change_address_format(self, registrations: list[UserRegistration]) -> None:
+        logger.info("Processing the changing address format for the "
+                    f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
+
+        for addr in registrations:
+            if (isinstance(addr.address, str) and
+                Utils().recognize_blockchain_network(addr.address) == "Ethereum"):
+                user_address = Web3.to_checksum_address(addr.address)
+                logger.info(f"Old format {addr.address = }. New format {user_address = }")
+                UserRegistrationRepository().update_registration_address(
+                    airdrop_window_id=self._window_id,
+                    old_address=addr.address,
+                    new_address=user_address
+                )
+                logger.info("Successfully updated")
+            else:
+                logger.info(f"Address = {addr.address} is not available for updating")
+
+    def process_change(self) -> str:
+        logger.info("Processing the address format changing for the "
+                    f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
+
+        registrations = self.receive_all_registrations()
+        if not registrations:
+            raise Exception("0 registrations received from db")
+        self.change_address_format(registrations)
 
         return (f"{len(registrations)} registrations changed on "
                 f"airdrop_id = {self._airdrop_id}, window_id = {self._window_id}")
