e79d3ed5dfaea91a85f3ee24f3e5e70e3da13777
mabredin <mbredin@bk.ru>
2025-06-27 17:29:35 +0300

    Clean up comment code in user_registraion_repo

--- airdrop/infrastructure/repositories/user_registration_repo.py
+++ airdrop/infrastructure/repositories/user_registration_repo.py
@@ -27,27 +27,6 @@ class UserRegistrationRepository(BaseRepository):
             self.session.rollback()
             raise e
 
-    # def airdrop_window_user_details(self, airdrop_window_id, address):
-    #     user_data = (
-    #         self.session.query(UserRegistration)
-    #         .join(
-    #             AirdropWindow,
-    #             AirdropWindow.id == UserRegistration.airdrop_window_id,
-    #         )
-    #         .filter(UserRegistration.airdrop_window_id == airdrop_window_id)
-    #         .filter(UserRegistration.address == address)
-    #         .filter(UserRegistration.registered_at != None)
-    #         .first()
-    #     )
-
-    #     user_details = None
-
-    #     if user_data is not None:
-    #         user_details = AirdropFactory.convert_airdrop_window_user_model_to_entity_model(
-    #             user_data)
-
-    #     return user_details
-
     def get_reject_reason(self, airdrop_window_id, address):
         registration = (
             self.session.query(UserRegistration.reject_reason)
